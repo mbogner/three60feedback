78376df5cebd55688bbe987f157d08e2cabffda4
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-08 21:33:53 +0200

    stop deletion

--- src/main/kotlin/dev/mbo/t60f/domain/round/FeedbackRoundTask.kt
+++ src/main/kotlin/dev/mbo/t60f/domain/round/FeedbackRoundTask.kt
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
-import java.time.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
 
 @Component
@@ -17,13 +16,6 @@ class FeedbackRoundTask(
 ) {
 
     private val log = logger()
-
-    @Transactional
-    @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
-    fun cleanup() {
-        log.info("cleanup rounds")
-        roundRepository.deleteByValidityIsBefore(Instant.now().plus(7, ChronoUnit.DAYS))
-    }
 
     @Transactional
     @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
