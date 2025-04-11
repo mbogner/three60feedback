beacd8a2b20ab50d63cb1028f092f965fc4a9dd6
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-11 19:10:38 +0200

    add auth test for my space

---
+++ src/test/kotlin/dev/mbo/t60f/domain/MyAuthTest.kt
@@ -0,0 +1,76 @@
+package dev.mbo.t60f.domain
+
+import dev.mbo.t60f.AbstractSpringBootTest
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class MyAuthTest : AbstractSpringBootTest() {
+
+    @Nested
+    inner class AnonymousAccess {
+
+        @Test
+        fun indexAuth() {
+            mockMvc().perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isFound)
+                .andExpect(redirectedUrlPattern("**/login"))
+        }
+
+        @Test
+        fun roundsAuth() {
+            mockMvc().perform(get("/my/rounds"))
+                .andDo(print())
+                .andExpect(status().isFound)
+                .andExpect(redirectedUrlPattern("**/login"))
+        }
+    }
+
+    @Nested
+    inner class UserAccess {
+
+        @Test
+        @WithMockUser(roles = ["USER"])
+        fun indexAsUser() {
+            mockMvc().perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isFound)
+                .andExpect(redirectedUrl("/my/rounds"))
+        }
+
+        @Test
+        @WithMockUser(roles = ["USER"])
+        fun roundsAsUser() {
+            mockMvc().perform(get("/my/rounds"))
+                .andDo(print())
+                .andExpect(status().isOk)
+        }
+    }
+
+    @Nested
+    inner class AdminAccess {
+        // admin has no rounds and no email
+
+        @Test
+        @WithMockUser(roles = ["ADMIN"])
+        fun indexAsAdmin() {
+            mockMvc().perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isForbidden)
+        }
+
+        @Test
+        @WithMockUser(roles = ["ADMIN"])
+        fun roundsAsAdmin() {
+            mockMvc().perform(get("/my/rounds"))
+                .andDo(print())
+                .andExpect(status().isForbidden)
+        }
+    }
+}
