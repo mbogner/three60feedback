5178198e12de5d9bfbed9c114fd51d3e0e5e512b
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-27 00:48:19 +0200

    bug: used join instead of left join

--- src/main/kotlin/dev/mbo/t60f/domain/round/FeedbackRoundRepository.kt
+++ src/main/kotlin/dev/mbo/t60f/domain/round/FeedbackRoundRepository.kt
@@ -24,8 +24,8 @@ interface FeedbackRoundRepository : JpaRepository<FeedbackRound, UUID> {
     @Query(
         """
         select r from FeedbackRound r
-            join fetch r.givers g
-            join fetch g.messages
+            left join fetch r.givers g
+            left join fetch g.messages
         where r.id=:feedbackRoundId
         order by r.createdAt desc
         """
